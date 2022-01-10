ba4214ab0e23e620a9300d789c5c0846b6a12389
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-09 23:07:35 -0600

    Add/several edits

---
+++ name.py
@@ -0,0 +1,16 @@
+name = "tommy lee jones"
+print(name.title())
+
+name = "Linda Fiorentino"
+print(name.upper())
+print(name.lower())
+
+first_name = "will"
+last_name = "smith"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
