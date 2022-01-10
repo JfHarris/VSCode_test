3e46fb720f2573e09a31345877fcec2272ccd00c
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-10 00:32:43 -0600

    Add/edit

---
+++ motorcycles.py
@@ -0,0 +1,13 @@
+#change elements in list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# add element to end of list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.append('ducati')
+print(motorcycles)
+# append adds to the end of the list without affecting other elements
+
