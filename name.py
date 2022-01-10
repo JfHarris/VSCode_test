811a3b445774bf9a4994be55cc1b502e757569ad
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-09 23:10:11 -0600

    Added comments

--- name.py
+++ name.py
@@ -1,16 +1,17 @@
+# using title function
 name = "tommy lee jones"
 print(name.title())
-
+# using upper and lower functions
 name = "Linda Fiorentino"
 print(name.upper())
 print(name.lower())
-
+#concat two vars
 first_name = "will"
 last_name = "smith"
 full_name = first_name + " " + last_name
 print(full_name)
-
+# adding title function to concatted string vars
 print("Hello, " + full_name.title() + "!")
-
+# storing message in var to simplify reading
 message = "Hello, " + full_name.title() + "!"
 print(message)
