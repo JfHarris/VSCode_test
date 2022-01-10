584bef2c55593fbb5325212e41766de5a450b859
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-09 23:18:36 -0600

    Add/several edits

---
+++ whitespace.py
@@ -0,0 +1,11 @@
+#simple print
+print("Python")
+
+# print with tab
+print("\tPython")
+
+#print with newline
+print("Languages:\nPython\nC\nJavaScript")
+
+#print with newline and tabs
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
