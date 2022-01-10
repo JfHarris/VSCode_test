090785b851349efd0faa2bc0467fc76a576d310d
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-09 23:31:23 -0600

    Add/edit

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+#simple exercise to show that using double quotes can help to avoid
+# syntax errors. If message was enclosed in single quotes/apostrophes, it would
+# cause an error when it reached the apostrophe in Python's
