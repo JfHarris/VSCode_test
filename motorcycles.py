92ae3033db30b869b937920f7cba4b2e676684c9
JfHarris <83927970+JfHarris@users.noreply.github.com>
2022-01-12 23:36:59 -0600

    Added remove methods

--- motorcycles.py
+++ motorcycles.py
@@ -4,10 +4,76 @@ print(motorcycles)
 
 motorcycles[0] = 'ducati'
 print(motorcycles)
+#replces honda with ducati
 
 # add element to end of list
 motorcycles = ['honda', 'yamaha', 'suzuki']
 motorcycles.append('ducati')
 print(motorcycles)
 # append adds to the end of the list without affecting other elements
+# append() method makes building lists dynamically easier in some cases too
+motorcycles = []
 
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+# creates a replica of first list in this file
+
+# insert() method
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+print("\n")
+# inserts value 'ducati' at position 0 of list.
+# shifts position of other values one position to right
+
+#remove items using del
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+print("\n")
+# remove item at position 0 of list motorcycles
+# can change position value to change which item is deleted
+
+#remove items using pop method
+# pop removes last item of a list but lets you work with it after removing it
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+# example of use case
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+# pop items from any position in list by using index of item
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title() + '.')
+print("\n")
+# del does not allow for use after it. Pop does
+
+# using remove method
+# allows us to remove by value of item
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+print("\n")
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
