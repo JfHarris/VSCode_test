557368a860ecef5d856b37c07dd8dfa783ff0da4
jfharris <jfharris1983@gmail.com>
2022-01-22 01:33:38 -0600

    Add/Edit

---
+++ List_Creation_And_Manipulation/magicians.py
@@ -0,0 +1,22 @@
+# using for loop to print list
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+print("\n")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + "\n")
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + "\n")
+
+print("Thank you, everyone. That was a great magic show")
+print("\n")
