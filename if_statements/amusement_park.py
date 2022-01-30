59382c51f0a17a5b9ebc1c4785775e71675ea8fb
jfharris <jfharris1983@gmail.com>
2022-01-29 23:19:21 -0600

    Added amusement park and edited toppings

---
+++ if_statements/amusement_park.py
@@ -0,0 +1,55 @@
+# admission for anyone under age 4 is free
+# admission between ages 4 and 18 is $5
+# admission for ages 18+ is $10
+
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+# more efficient way
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
+
+# added elif
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
+
+# replace else with elif for seniors
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
--- if_statements/toppings.py
+++ if_statements/toppings.py
@@ -1,10 +1,81 @@
-requested_topping = 'mushrooms'
-
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+# requested_topping = 'mushrooms'
+#
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
 
 # >>> requested_toppings = ['mushrooms', 'onions', 'pineapple']
 # >>> 'mushrooms' in requested_toppings
 # True
 # >>> 'pepperoni' in requested_toppings
 # False
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+print("\n")
+
+# will only run first if because it returns true and negates elifs
+# requested_toppings = ['mushrooms', 'extra cheese']
+
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms.")
+# elif 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# elif 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+
+# print("\nFinished making your pizza!")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+print("\n")
+
+# checking for missing items
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are all out of green peppers right now.")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+print("\n")
+
+# checking empty list
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+else:
+    print("Are you sure you want a plain pizza?")
+print("\n")
+
+# using multiple lists
+
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                        'pepperoni', 'pineaplle', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
