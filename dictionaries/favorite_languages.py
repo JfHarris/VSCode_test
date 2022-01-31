fbb3928818b3ccdd7fbc3346da1d4ece23b7c346
jfharris <jfharris1983@gmail.com>
2022-01-31 00:20:38 -0600

    Update

--- dictionaries/favorite_languages.py
+++ dictionaries/favorite_languages.py
@@ -8,3 +8,96 @@ favorite_languages = {
 print("Sarah's favorite language is " +
     favorite_languages['sarah'].title() +
     ".")
+print("\n")
+
+# loop through dict
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+    language.title() + ".")
+print("\n")
+
+# loop using keys() method
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+for name in favorite_languages.keys():
+    print(name.title())
+print("\n")
+
+# print when a name is found
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        print(" Hi " + name.title() +
+        ", I see your favorite language is " +
+        favorite_languages[name].title() + "!")
+print("\n")
+
+# if missing a key
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+print("\n")
+
+# looping through keys in order
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+print("\n")
+
+# loop using values
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+print("\n")
+
+# using set to remove repeated values
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
