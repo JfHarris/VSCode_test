782bb3717e4da1070e3cd33f250980090bd00d5d
jfharris <jfharris1983@gmail.com>
2022-02-26 02:39:35 -0600

    Add

---
+++ dictionaries/favorite_languages2.py
@@ -0,0 +1,26 @@
+favorite_languages = {
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskell'],
+}
+
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+
+# Jen's favorite languages are:
+#        Python
+#        Ruby
+
+# Sarah's favorite languages are:
+#         C
+
+# Edward's favorite languages are:
+#         Ruby
+#         Go
+
+# Phil's favorite languages are:
+#         Python
+#         Haskell
